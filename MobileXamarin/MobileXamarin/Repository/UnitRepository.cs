4af4d0158c5b1c6698292138d6264553e077ae30
Piotr Makowiec <piotrmakowiec@outlook.com>
2019-03-17 13:34:45 +0100

    Unit repository

---
+++ MobileXamarin/MobileXamarin/Repository/UnitRepository.cs
@@ -0,0 +1,37 @@
+﻿// UnitRepository.cs
+// All rights reserved
+// Piotr Makowiec 17-03-2019
+
+using System.Collections.Generic;
+using MobileXamarin.Enums;
+
+namespace MobileXamarin.Repository
+{
+    public static class UnitRepository
+    {
+        private static IDictionary<Units, string> unitRepresentation;
+
+        private static void Initialize()
+        {
+            unitRepresentation = new Dictionary<Units, string>
+            {
+                {Units.Hour, "h" },
+                {Units.Gram, "g" },
+                {Units.Kilogram, "Kg" },
+                {Units.Kilometer, "Km" },
+                {Units.Meter, "m" },
+                {Units.KilometerPerHour, "Km/h" },
+                {Units.MeterForSecond, "m/s" },
+                {Units.Second, "s" }
+            };
+        }
+
+        public static string GetUnitString(Units unit)
+        {
+            Initialize();
+            var canGetValue = unitRepresentation.TryGetValue(unit, out var representation);
+
+            return canGetValue ? representation : string.Empty;
+        }
+    }
+}
