3df582d86aba3c699e72dc32a5e4ef49a3d8f1ce
urszulalis <urszula.lis95@gmail.com>
2019-05-05 20:41:00 +0200

    Change App Name

--- MobileXamarin/MobileXamarin.Android/MainActivity.cs
+++ MobileXamarin/MobileXamarin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MobileXamarin.Android
 {
-    [Activity(Label = "Android", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Equation Solver", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
